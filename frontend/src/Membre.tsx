3184be02d4ae6260ba025bf38e0a5e77756c3965
Ant-sai <antoine.saintlouis@gmail.com>
2025-10-04 17:29:08 +0200

    update vps

--- frontend/src/Membre.tsx
+++ frontend/src/Membre.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export type User = {
   id: number;
